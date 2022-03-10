cc499ad8f8b71a27cdf632875f82e837a5017d80
Ivcota <ivcotad@gmail.com>
2022-03-10 16:45:34 -0700

    apollo server running

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,10 +1,24 @@
 import express from "express";
-
 import { ApolloServer } from "apollo-server-express";
+import { schema } from "./schema";
+import { context } from "./context";
+
+const app = express();
 
 const startServer = async () => {
-  const app = express();
-  //   const server = new ApolloServer();
+  const server = new ApolloServer({ schema, context });
+  await server.start();
+  server.applyMiddleware({ app });
 
-  //   await server.start();
+  await new Promise((resolve) => {
+    app.listen(4000, () => {
+      console.log(
+        4000,
+        `Server is running at http://localhost:4000${server.graphqlPath}`
+      );
+    });
+    resolve as any;
+  });
 };
+
+startServer();
