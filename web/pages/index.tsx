cd9c104c01803ede22d66d5e93bc1875d26c0260
Ivcota <ivcotad@gmail.com>
2022-03-20 17:07:51 -0700

    change height of button

--- web/pages/index.tsx
+++ web/pages/index.tsx
@@ -6,7 +6,7 @@ const Home: NextPage = () => {
     <div className="flex flex-col items-center justify-center min-h-screen gap-4">
       <h1 className="text-3xl font-logo">Portfolio Stacker</h1>
       <h3>Quickly Get Your Work Seen</h3>
-      <button className="py-1 rounded-sm dark:text-steel-900 dark:bg-steel-100 w-52">
+      <button className="py-2 rounded-sm dark:text-steel-900 dark:bg-steel-100 w-52 dark:focus:bg-steel-300">
         Get Started
       </button>
     </div>
